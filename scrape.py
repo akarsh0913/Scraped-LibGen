63af1f50d8db62e287322a01f01ba404ae3125c7
Akarsh Mishra <134067749+akarsh0913@users.noreply.github.com>
2023-08-13 05:02:45 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,11 +6,11 @@ tags = entry.split()
 formatted_tags = '+'.join(tags)
 base_url = "https://libgen.li/index.php?req="
 query_url = f'{base_url}{formatted_tags}'
-page = requests.get(query_url) # Getting page HTML through request
-soup = BeautifulSoup(page.content, 'html.parser') # Parsing content using beautifulsoup
+page = requests.get(query_url)
+soup = BeautifulSoup(page.content, 'html.parser')
  
-links = soup.select("table tbody tr td a") # Selecting all of the anchors with titles
-first1000 = links[:1000] # Keep only the first 10 anchors
+links = soup.select("table tbody tr td a")
+first1000 = links[:1000]
 for anchor in first1000:
     href = anchor.get('href')
     if href and 'libgen.rocks' in href:
