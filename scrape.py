affe921bbcf9b2ad5c7950315359d190350ac435
Thors <ashutoshtrirp@gmail.com>
2023-08-17 03:18:19 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 entry = input("Enter book name: ")
 tags = entry.split()
 formatted_tags = '+'.join(tags)
-base_url = "https://libgen.li/index.php?req="
+base_url = "https://libgen.si/index.php?req="
 query_url = f'{base_url}{formatted_tags}'
 page = requests.get(query_url)
 soup = BeautifulSoup(page.content, 'html.parser')
  
-links = soup.select("table tbody tr td a")
+links = soup.select("table tr td a")
 first1000 = links[:1000]
 for anchor in first1000:
     href = anchor.get('href')
-    if href and 'libgen.rocks' in href:
+    if href and 'library.lol' in href:
         print(f'href: {href}')
         print('-'*40)
